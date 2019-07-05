1904b7328ca2bd5181d44fba33ed1e947920c969
gosho-kazuya <ketsume0211@gmail.com>
2019-07-06 04:04:34 +0900

    getting used to rust

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,27 @@
-use serde_json::{Result, Value};
+use serde_json::{Value};
 use std::fs;
+use std::io;
 
 fn main() {
     let contents = fs::read_to_string("./package.json")
         .expect("Something went wrong reading the file");
 
-    println!("With text:\n{}", contents);
+    let v = parse(&contents);
+
+    let v = match v {
+        Ok(strings) => strings,
+        Err(_e) => panic!("No script"),
+    };
+
+    println!("scripts: \n{}", v[0]);
 
     // untyped_example();
-
-    let v: Value = serde_json::from_str(contents);
-
     // let v: Value = parse(contents);
-
     // println!("scripts: \n{}", v["scripts"]);
 }
 
-fn parse(contents: String) -> Result<()> {
-    // Parse the string of data into serde_json::Value.
+fn parse(contents: &str) -> Result<Vec<String>, io::Error> {
     let v: Value = serde_json::from_str(contents)?;
 
-    // Access parts of the data by indexing with square brackets.
-    println!("Please call {} at the number {}", v["name"], v["phones"][0]);
-
-    Ok(())
-}
-
-fn untyped_example() -> Result<()> {
-    // Some JSON input data as a &str. Maybe this comes from the user.
-    let data = r#"
-        {
-            "name": "John Doe",
-            "age": 43,
-            "phones": [
-                "+44 1234567",
-                "+44 2345678"
-            ]
-        }"#;
-
-    // Parse the string of data into serde_json::Value.
-    let v: Value = serde_json::from_str(data)?;
-
-    // Access parts of the data by indexing with square brackets.
-    println!("Please call {} at the number {}", v["name"], v["phones"][0]);
-
-    Ok(())
+    Ok(vec![v["scripts"]["test"].to_string()])
 }
