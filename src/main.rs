df497053c4f31e4c39132152382d9ba9d246367f
gosho-kazuya <ketsume0211@gmail.com>
2019-07-06 13:19:22 +0900

    refacor

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,12 @@ fn main() {
     let contents = fs::read_to_string("./package.json")
         .expect("Something went wrong reading the file");
 
-    let v = parse(&contents);
-
-    let v = match v {
+    let v = match parse(&contents) {
         Ok(strings) => strings,
         Err(_e) => panic!("No script"),
     };
 
     println!("scripts: \n{}", v[0]);
-
-    // untyped_example();
-    // let v: Value = parse(contents);
-    // println!("scripts: \n{}", v["scripts"]);
 }
 
 fn parse(contents: &str) -> Result<Vec<String>, io::Error> {
